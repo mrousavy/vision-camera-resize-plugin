c9c08837d6e63fb41879201b73250033debe9b0b
Marc Rousavy <marcrousavy@hotmail.com>
2021-06-07 13:54:22 +0200

    Update VisionCameraResizePlugin.m

--- ios/VisionCameraResizePlugin.m
+++ ios/VisionCameraResizePlugin.m
@@ -9,43 +9,24 @@
 @import Foundation;
 @import AVFoundation;
 
-// Example for an Objective-C Frame Processor plugin
-
-@interface QRCodeFrameProcessorPluginObjC : NSObject
-
-+ (MLKResizePlugin*) labeler;
-
+@interface FrameProcessorResizePlugin : NSObject
 @end
 
-@implementation QRCodeFrameProcessorPluginObjC
+@implementation FrameProcessorResizePlugin
 
-+ (MLKResizePlugin*) labeler {
-  static MLKResizePlugin* labeler = nil;
-  if (labeler == nil) {
-    MLKResizePluginOptions* options = [[MLKResizePluginOptions alloc] init];
-    labeler = [MLKResizePlugin ResizePluginWithOptions:options];
+static inline id resize(CMSampleBufferRef buffer, NSArray* arguments) {
+  NSNumber* width = [arguments objectAtIndex:0];
+  NSNumber* height = [arguments objectAtIndex:1];
+  if (width == nil || height == nil) {
+    return nil;
   }
-  return labeler;
+  if (width.intValue <= 0 || height.intValue <= 0) {
+    return nil;
+  }
+  
+  return buffer;
 }
 
-static inline id labelImage(CMSampleBufferRef buffer, NSArray* arguments) {
-  MLKVisionImage *image = [[MLKVisionImage alloc] initWithBuffer:buffer];
-  image.orientation = UIImageOrientationRight; // <-- TODO: is mirrored?
-
-  NSError* error;
-  NSArray<MLKImageLabel*>* labels = [[QRCodeFrameProcessorPluginObjC labeler] resultsInImage:image error:&error];
-
-  NSMutableArray* results = [NSMutableArray arrayWithCapacity:labels.count];
-  for (MLKImageLabel* label in labels) {
-    [results addObject:@{
-      @"label": label.text,
-      @"confidence": [NSNumber numberWithFloat:label.confidence]
-    }];
-  }
-
-  return results;
-}
-
-VISION_EXPORT_FRAME_PROCESSOR(labelImage)
+VISION_EXPORT_FRAME_PROCESSOR(resize)
 
 @end
