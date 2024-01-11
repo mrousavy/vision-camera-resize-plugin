fbcd4e8cbc8e02ba732df894867302e3086b91ef
Marc Rousavy <me@mrousavy.com>
2024-01-11 18:13:57 +0100

    fix: add deps

--- example/babel.config.js
+++ example/babel.config.js
@@ -13,5 +13,6 @@ module.exports = {
         },
       },
     ],
+    'react-native-worklets-core/plugin',
   ],
 };
--- ios/ResizePlugin.mm
+++ ios/ResizePlugin.mm
@@ -25,7 +25,7 @@
   self = [super init];
   NSNumber* width = (NSNumber*) options[@"targetWidth"];
   NSNumber* height = (NSNumber*) options[@"targetHeight"];
-  NSAssert(width != nil && height != nil, "targetWidth or targetHeight are required parameters!");
+  NSAssert(width != nil && height != nil, @"targetWidth or targetHeight are required parameters!");
   NSNumber* channelSize = (NSNumber*) options[@"channelSize"];
   
   _frameResizer = std::make_unique<FrameResizer>((size_t) width.intValue,
@@ -37,7 +37,7 @@
 - (id)callback:(Frame*)frame withArguments:(NSDictionary*)arguments {
   CVPixelBufferRef pixelBuffer = CMSampleBufferGetImageBuffer(frame.buffer);
   
-  vImage_Buffer& resizedFrame = _frameResizer->resizeFrame(pixelBuffer);
+  const vImage_Buffer& resizedFrame = _frameResizer->resizeFrame(pixelBuffer);
   // code goes here
   return @[];
 }
