7632d658a6169c15c4bbde04c105d474d869d189
Marc Rousavy <me@mrousavy.com>
2024-01-29 11:20:09 +0100

    Add scripts

---
+++ scripts/check-all.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Linting Kotlin code.."
+./scripts/ktlint.sh
+
+echo "Formatting C++ code.."
+./scripts/clang-format.sh
+
+echo "Linting JS/TS code.."
+yarn lint --fix
+yarn typescript
+
+echo "All done!"
