0ecdeeb1790799f5a7c6d566e5dbc02763628988
pweglik <przemek.weglik@onet.pl>
2024-04-22 20:15:11 +0200

    Add comments explaining confusing function names

--- android/src/main/cpp/ResizePlugin.cpp
+++ android/src/main/cpp/ResizePlugin.cpp
@@ -276,10 +276,12 @@ FrameBuffer ResizePlugin::convertARGBBufferTo(FrameBuffer frameBuffer, PixelForm
       // do nothing, we're already in ARGB
       return frameBuffer;
     case RGB:
+      // RAW is [B, G, R] in libyuv memory layout
       error = libyuv::ARGBToRAW(frameBuffer.data(), frameBuffer.bytesPerRow(), destination.data(), destination.bytesPerRow(),
                                 destination.width, destination.height);
       break;
     case BGR:
+      // RGB24 is [B, G, R] in libyuv memory layout
       error = libyuv::ARGBToRGB24(frameBuffer.data(), frameBuffer.bytesPerRow(), destination.data(), destination.bytesPerRow(),
                                   destination.width, destination.height);
       break;
