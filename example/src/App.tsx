27ca857780c100ebc5b7134216510b1bc85123af
rodgomesc <rodgomesc@gmail.com>
2024-05-03 06:44:29 -0300

    provide a easier way to preview image resizer output

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -8,10 +8,23 @@ import {
   useFrameProcessor,
 } from 'react-native-vision-camera';
 import { useResizePlugin } from 'vision-camera-resize-plugin';
+import { useSharedValue } from 'react-native-reanimated';
+import {
+  Skia,
+  AlphaType,
+  ColorType,
+  Image,
+  SkData,
+  Canvas,
+  SkImage,
+} from '@shopify/react-native-skia';
+
+const SIZE = 256;
 
 export default function App() {
   const permission = useCameraPermission();
   const device = useCameraDevice('back');
+  const previewImg = useSharedValue<SkImage | null>(null);
 
   React.useEffect(() => {
     permission.requestPermission();
@@ -19,53 +32,76 @@ export default function App() {
 
   const plugin = useResizePlugin();
 
-  const frameProcessor = useFrameProcessor((frame) => {
-    'worklet';
-    console.log(frame.toString());
-
-    const start = performance.now();
-
-    const targetWidth = 250;
-    const targetHeight = 250;
-
-    const result = plugin.resize(frame, {
-      scale: {
-        width: targetWidth,
-        height: targetHeight,
+  const handleSkiaImage = Worklets.createRunOnJS((data: SkData) => {
+    const img = Skia.Image.MakeImage(
+      {
+        width: SIZE,
+        height: SIZE,
+        alphaType: AlphaType.Opaque,
+        colorType: ColorType.RGBA_8888,
       },
-      pixelFormat: 'rgba',
-      dataType: 'uint8',
-      rotation: '90deg',
-      mirror: true,
-    });
-    console.log(
-      result[Math.round(result.length / 2) + 0],
-      result[Math.round(result.length / 2) + 1],
-      result[Math.round(result.length / 2) + 2],
-      result[Math.round(result.length / 2) + 3],
-      '(' + result.length + ')'
+      data,
+      SIZE * 4
     );
 
-    const end = performance.now();
+    previewImg.value = img;
+  });
 
-    console.log(
-      `Resized ${frame.width}x${frame.height} into 100x100 frame (${
-        result.length
-      }) in ${(end - start).toFixed(2)}ms`
-    );
-  }, []);
+  const frameProcessor = useFrameProcessor(
+    (frame) => {
+      'worklet';
+
+      const start = performance.now();
+
+      const result = plugin.resize(frame, {
+        scale: {
+          width: SIZE,
+          height: SIZE,
+        },
+        pixelFormat: 'rgba',
+        dataType: 'uint8',
+        rotation: '90deg',
+        mirror: true,
+      });
+
+      const data = Skia.Data.fromBytes(result);
+
+      handleSkiaImage(data);
+      const end = performance.now();
+
+      console.log(
+        `Resized ${frame.width}x${frame.height} into 100x100 frame (${
+          result.length
+        }) in ${(end - start).toFixed(2)}ms`
+      );
+    },
+    [handleSkiaImage]
+  );
 
   return (
     <View style={styles.container}>
       {permission.hasPermission && device != null && (
         <Camera
           device={device}
+          enableFpsGraph
           style={StyleSheet.absoluteFill}
           isActive={true}
           pixelFormat="yuv"
           frameProcessor={frameProcessor}
         />
       )}
+      <View style={styles.canvasWrapper}>
+        <Canvas style={{ width: SIZE, height: SIZE }}>
+          <Image
+            image={previewImg}
+            x={0}
+            y={0}
+            width={SIZE}
+            height={SIZE}
+            fit="cover"
+          />
+        </Canvas>
+      </View>
     </View>
   );
 }
@@ -80,5 +116,10 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
     marginVertical: 20,
+  },
+  canvasWrapper: {
+    position: 'absolute',
+    bottom: 80,
+    left: 20,
   },
 });
